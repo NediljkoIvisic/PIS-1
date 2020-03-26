59b0e37551b9abf5baf3962a71c127730f34d393
NediljkoIvisic <62703844+NediljkoIvisic@users.noreply.github.com>
2020-03-26 17:10:32 +0100

    Izmjena programa od strane kolaboratora

--- Vjezba1/Program.cs
+++ Vjezba1/Program.cs
@@ -13,5 +13,10 @@ namespace Vjezba1
             //Prva promjena
             Console.Write("Prva izmjena koda u Program.cs.\n");
         }
+        
+        void kolaborator()
+        {
+            //Izmjena programa od strane kolaboratora
+        }
     }
 }
